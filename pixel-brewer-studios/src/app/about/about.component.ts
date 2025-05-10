69add522447e3879a0697ed058ec69d93062c1d5
Pixelbrewer <pixelbrewerstudios@gmail.com>
2025-05-09 23:31:03 -0700

    Adding content and basic styling

--- pixel-brewer-studios/src/app/about/about.component.ts
+++ pixel-brewer-studios/src/app/about/about.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { ExperienceComponent } from '../experience/experience.component';
+import { GalleryComponent } from '../gallery/gallery.component';
 
 @Component({
   selector: 'app-about',
-  imports: [ExperienceComponent],
+  imports: [ExperienceComponent, GalleryComponent],
   templateUrl: './about.component.html',
 })
 export class AboutComponent {}
