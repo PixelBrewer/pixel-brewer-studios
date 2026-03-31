53bc6bf311435c17cf4525efdf447b30c7bdedde
PixelBrewer <PixelBrewer@protonmail.com>
2026-03-30 17:47:29 -0700

    Adding basic contact form

--- pixel-brewer-client/src/app/contact/contact.ts
+++ pixel-brewer-client/src/app/contact/contact.ts
@@ -1,13 +1,27 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { FormGroup, FormControl, Validators, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-contact',
   changeDetection: ChangeDetectionStrategy.OnPush,
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './contact.html',
 })
 export class Contact {
   email: string = 'PixelBrewer@protonmail.com';
   githubUrl: string = 'https://www.github.com/PixelBrewer';
   linkedInUrl: string = 'https://www.linkedin.com/in/alexanderjblair/';
+
+  contactForm = new FormGroup({
+    name: new FormControl('', Validators.required),
+    email: new FormControl('', Validators.required),
+    subject: new FormControl('', Validators.required),
+    message: new FormControl('', [Validators.required, Validators.minLength(10)]),
+  });
+
+  constructor() {}
+
+  onSubmit() {
+    console.log(this.contactForm.value);
+  }
 }
